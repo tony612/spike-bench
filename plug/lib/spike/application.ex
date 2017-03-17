2f0eb1fe10c0fe6a1a9b5d157a430a9f21c3bc61
Tony Han <h.bing612@gmail.com>
2017-03-17 22:27:47 +0800

    add plug

---
+++ plug/lib/spike/application.ex
@@ -0,0 +1,22 @@
+defmodule Spike.Application do
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    # Define workers and child supervisors to be supervised
+    children = [
+      # Starts a worker by calling: Spike.Worker.start_link(arg1, arg2, arg3)
+      # worker(Spike.Worker, [arg1, arg2, arg3]),
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Spike.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
---
+++ plug/test/spike_test.exs
@@ -0,0 +1,8 @@
+defmodule SpikeTest do
+  use ExUnit.Case
+  doctest Spike
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
